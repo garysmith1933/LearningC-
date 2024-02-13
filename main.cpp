efbf89776057ffb4b542c0e170f71424656259d5
Gary Smith <garysmith193358@gmail.com>
2024-02-13 07:48:04 -0500

    added comment about std::cout

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 int main() {
-  std::cout << "Hello World";
+  int file_size = 0;
+  double sales = 9.99;
+  // equivalent to print in python, console.log, system.out.print
+  std::cout << file_size;
   return 0;
 }
