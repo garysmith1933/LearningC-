52c7517442ab1f9f5038efe26116e79bca0fa024
Gary Smith <garysmith193358@gmail.com>
2024-02-13 14:55:37 -0500

    created a program that calucates and returns price with taxes

--- main.cpp
+++ main.cpp
@@ -1,19 +1,38 @@
 #include <iostream>
+using namespace std;
+
+// int main() {
+//   int a = 1;
+//   int b = 2;
+
+//   int temp = b;
+//   // equivalent to print in python, console.log, system.out.print
+//   //<<std::endl is used to print on another line
+//   cout << "a = " << a << endl;
+//   cout << "b = " << b << endl;
+
+//   b = a;
+//   a = temp;
+
+//   // << output stream
+//   cout << "a = " << a << endl;
+//   cout << "b = " << b << endl;
+//   return 0;
+// }
+
 int main() {
-  int a = 1;
-  int b = 2;
+  double sales = 95000;
 
-  int temp = b;
-  // equivalent to print in python, console.log, system.out.print
-  //<<std::endl is used to print on another line
-  std::cout << "a = " << a << std::endl;
-  std::cout << "b = " << b << std::endl;
+  float state_tax = sales * 0.04;
+  float county_tax = sales * 0.02;
 
-  b = a;
-  a = temp;
+  cout << "Current price is $" << sales << endl;
 
-  // << output stream
-  std::cout << "a = " << a << std::endl;
-  std::cout << "b = " << b << std::endl;
+  cout << "With sales tax $" << sales + state_tax << endl;
+  cout << "With county tax $" << sales + county_tax << endl;
+
+  float sales_total = sales + state_tax + county_tax;
+
+  cout << "Total price is $" << sales_total;
   return 0;
 }
